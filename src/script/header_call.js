96688975d3c5517866a41eaacb31ffe66864a670
lyd1040 <skswngussla@naver.com>
2023-06-05 18:15:54 +0900

    save

--- src/script/header_call.js
+++ src/script/header_call.js
@@ -1,9 +1,43 @@
-const xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("header").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", "/src/view/header/header.html");
-  xhttp.send();
+const headerId = document.getElementById("header");
+const footerId = document.getElementById("footer");
+const body = document.querySelector("body");
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  headerFooterExec();
+});
+
+/* header,footer load */
+const headerFooterExec = () => {
+  headerFooterPrint().then(() => {
+    headerFooterJs();
+  });
+};
+
+const headerFooterPrint = async () => {
+  const headerData = await getHeader();
+/* 
+  const footerData = await getFooter();
+ */
+  headerId.innerHTML = headerData;
+/* 
+  footerId.innerHTML = footerData;
+   */
+};
+
+const getHeader = () => {
+  const response = fetch("/src/view/header/header.html");
+  return response.then((res) => res.text());
+};
+/* 
+const getFooter = () => {
+  const response = fetch("/src/view/footer/footer.html");
+  return response.then((res) => res.text());
+};
+ */
+
+const headerFooterJs = () => {
+  let createScript = document.createElement("script");
+  createScript.setAttribute("src", "/src/script/header.js");
+  body.appendChild(createScript);
+};
