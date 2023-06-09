b648d7f7ef56e8374bc56b0c09d0384180c177f5
lyd1040 <skswngussla@naver.com>
2023-06-09 13:37:48 +0900

    save

--- src/script/header_footer_call.js
+++ src/script/header_footer_call.js
@@ -16,30 +16,25 @@ const headerFooterExec = () => {
 
 const headerFooterPrint = async () => {
   const headerData = await getHeader();
-/* 
   const footerData = await getFooter();
- */
   headerId.innerHTML = headerData;
-/* 
   footerId.innerHTML = footerData;
-   */
 };
 
 const getHeader = () => {
   /* "/UNIVERSITY_moblie_pc_web_app/UNIVERSITY/src/view/header/header.html" */
-  const response = fetch("/src/view/header/header.html");
+  const response = fetch("/UNIVERSITY_moblie_pc_web_app/UNIVERSITY/src/view/header/header.html");
   return response.then((res) => res.text());
 };
-/* 
+
 const getFooter = () => {
-  const response = fetch("/src/view/footer/footer.html");
+  const response = fetch("/UNIVERSITY_moblie_pc_web_app/UNIVERSITY/src/view/footer/footer.html");
   return response.then((res) => res.text());
 };
- */
 
 const headerFooterJs = () => {
   let createScript = document.createElement("script");
   /* /UNIVERSITY_moblie_pc_web_app/UNIVERSITY/src/script/header.js */
-  createScript.setAttribute("src", "/src/script/header.js");
+  createScript.setAttribute("src", "/UNIVERSITY_moblie_pc_web_app/UNIVERSITY/src/script/header.js");
   body.appendChild(createScript);
 };
