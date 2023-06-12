c54c3cee28ccb8a3ae1106cf8c012b470925be9e
lyd1040 <skswngussla@naver.com>
2023-06-12 18:18:03 +0900

    save

--- src/script/header_footer_call.js
+++ src/script/header_footer_call.js
@@ -23,18 +23,18 @@ const headerFooterPrint = async () => {
 
 const getHeader = () => {
   /* "/UNIVERSITY_moblie_pc_web_app/UNIVERSITY/src/view/header/header.html" */
-  const response = fetch("/UNIVERSITY_moblie_pc_web_app/UNIVERSITY/src/view/header/header.html");
+  const response = fetch("/src/view/header/header.html");
   return response.then((res) => res.text());
 };
 
 const getFooter = () => {
-  const response = fetch("/UNIVERSITY_moblie_pc_web_app/UNIVERSITY/src/view/footer/footer.html");
+  const response = fetch("/src/view/footer/footer.html");
   return response.then((res) => res.text());
 };
 
 const headerFooterJs = () => {
   let createScript = document.createElement("script");
   /* /UNIVERSITY_moblie_pc_web_app/UNIVERSITY/src/script/header.js */
-  createScript.setAttribute("src", "/UNIVERSITY_moblie_pc_web_app/UNIVERSITY/src/script/header.js");
+  createScript.setAttribute("src", "/src/script/header.js");
   body.appendChild(createScript);
 };
