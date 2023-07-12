a3484b8ca1df09e0bfe38f145460e5d4632c21e8
lyd1040 <skswngussla@naver.com>
2023-07-12 18:19:49 +0900

    save

--- src/view/Login/login.js
+++ src/view/Login/login.js
@@ -1,20 +1,21 @@
 window.addEventListener('load',()=>{
     firebaseauthentication();
+    loading_show();
 })
 
 function firebaseauthentication(){
-    var emailInput = document.getElementById('email-input');
-    var passwordInput = document.getElementById('password-input');
-    var loginBtn = document.getElementById('login-btn');
+    const emailInput = document.getElementById('email-input');
+    const passwordInput = document.getElementById('password-input');
+    const loginBtn = document.getElementById('login-btn');
 
     // 로그인 버튼 클릭 이벤트 처리
     loginBtn.addEventListener('click', function() {
-        var email = emailInput.value;
-        var password = passwordInput.value;
+        const email = emailInput.value;
+        const password = passwordInput.value;
         firebase.auth().signInWithEmailAndPassword(email, password)
         .then(function(userCredential) {
         // 로그인 성공
-        var user = firebase.auth().currentUser;
+        const user = firebase.auth().currentUser;
         if (user) {
             sessionStorage.setItem('admin-uid',user.uid);
         } else {
@@ -29,4 +30,8 @@ function firebaseauthentication(){
         });
     });
 
+}
+
+function loading_show(){
+    const loginBtn = document.getElementById('login-btn');
 }
